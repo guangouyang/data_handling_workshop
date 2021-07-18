cfb2aa6aea3775f3e292c25b2f9c2a2309792c14
guangouyang <guang.ouyang@gmail.com>
2021-07-18 16:55:13 +0800

    Add files via upload

---
+++ task4/script4.m
@@ -0,0 +1,21 @@
+
+
+load('D:\Dropbox\work\teaching\Data_handling_workshop\task4\EEG.mat');
+
+for j = 1:length(timepoints_freq)
+    epoch_freq(:,j) = data(timepoints_freq(j) - 30:timepoints_freq(j) + 150);
+end
+
+
+for j = 1:length(timepoints_rare)
+    epoch_rare(:,j) = data(timepoints_rare(j) - 30:timepoints_rare(j) + 150);
+end
+
+
+
+t_axis = linspace(-200,1000,size(epoch_freq,1));
+figure;plot(t_axis,mean(epoch_freq,2));hold on;plot(t_axis,mean(epoch_rare,2));
+xlabel('ms');
+ylabel('\muV');
+title('brain reponse');
+legend({'frequent','rare'});
