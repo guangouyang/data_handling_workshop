dc184669c292c5df322abba7a684dd13884cb67b
guangouyang <guang.ouyang@gmail.com>
2021-07-21 17:14:12 +0800

    Add files via upload

--- task5/script5.m
+++ task5/script5.m
@@ -2,6 +2,11 @@ load('D:\Dropbox\work\teaching\Data_handling_workshop\task5\eye_tracking.mat');
 
 figure;plot(eye_xy(:,1),eye_xy(:,2),'.');hold on;
 set(gca,'Ydir','reverse');
+
+
+
+
+
 %screen area
 plot([0 1366 1366 0 0],[0 0 768 768 0],'k'); 
 
@@ -26,10 +31,10 @@ w = 1366;h = 768;bins = 200;
 
 face = imread('D:\Dropbox\work\teaching\Data_handling_workshop\task5\face.jpg');
 hold on;imagesc(585:783,136:434,face);hold on;set(gca,'Ydir','reverse');
-
 axis([0,w,0,h]);
 
-w = 1366;h = 768;bins = 200;
+
+
 heat = hist3(eye_xy,'edges',{linspace(0,1366,bins),linspace(0,768,bins)});
 
 imagesc(linspace(1,w,bins),linspace(1,h,bins),heat','AlphaData',heat'/max(heat(:)));colorbar;
