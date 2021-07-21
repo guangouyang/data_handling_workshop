736293cfa03ca4415692f5d83ea685909f009dc4
guangouyang <guang.ouyang@gmail.com>
2021-07-21 17:15:36 +0800

    Add files via upload

--- misc.m
+++ misc.m
@@ -2,15 +2,17 @@
 
 
 figure;plot(randn(1000,1));
+
+
 figure;imagesc(randn(30,20));
 
 
 
 txt_content = importdata('D:\Dropbox\work\teaching\Data_handling_workshop\hey.txt');
 
+disp(txt_content);
 
 system('%systemroot%\system32\scrnsave.scr /s');
-
 
 
 system('notepad');
