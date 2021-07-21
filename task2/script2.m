3db865f585e3d1dfaf9d5d4d7bb523744680b661
guangouyang <guang.ouyang@gmail.com>
2021-07-21 17:15:10 +0800

    Add files via upload

--- task2/script2.m
+++ task2/script2.m
@@ -7,9 +7,30 @@ a = randn(10,10);
 a = randi([-100,100],10,10);
 
 
-%take the second row and plot it
-b = a(2,:);figure;plot(b);
 
+%access information in a matrix
+
+a(2,3)
+
+
+a(4,:)
+
+
+a(3:8,:)
+
+
+a([2,5,6],:)
+
+
+%take the second row and plot it
+b = a(2,:);
+figure;plot(b);
+
+%swap the rows
+a([2,8],:) = a([8,2],:);
+
+
+%change information
 a = -a;
 
 %change all the positive values to zero
@@ -29,4 +50,5 @@ end
 a(a>0) = 0;
 
 
+%many others, e.g., sorting, transposing, transformations, etc...
 
